0eb4d4370e5d4b036013f48fda6a37304baacbca
Aaron Robinson <aaron.h.robinson@gmail.com>
2025-03-07 22:14:26 -0600

    Initial commit

---
+++ package/ir_relay_daemon/ir_relay_daemon.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# ir_relay_daemon
+#
+################################################################################
+
+IR_RELAY_DAEMON_VERSION = 1.0.1
+IR_RELAY_DAEMON_SOURCE = v$(IR_RELAY_DAEMON_VERSION).tar.gz
+IR_RELAY_DAEMON_SITE = https://github.com/arobinson434/IrRelayDaemon/archive/refs/tags
+IR_RELAY_DAEMON_CONF_OPTS = -DCMAKE_INSTALL_PREFIX=/
+IR_RELAY_DAEMON_DEPENDENCIES = boost protobuf libgpiod2
+
+define IR_RELAY_DAEMON_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 $(IR_RELAY_DAEMON_PKGDIR)/S99IrRelayDaemon \
+		$(TARGET_DIR)/etc/init.d/S99IrRelayDaemon
+	$(INSTALL) -D -m 644 $(IR_RELAY_DAEMON_PKGDIR)/IrRelayDaemon.defaults \
+		$(TARGET_DIR)/etc/default/IrRelayDaemon
+endef
+
+$(eval $(cmake-package))
